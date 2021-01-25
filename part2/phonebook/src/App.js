6d8b4c70875064213a66eb9c54397c7cf9c5debc
wusphinx <wu.sphinx@gmail.com>
2021-01-25 20:35:05 +0800

    done 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,49 @@
 import React, { useState } from 'react'
 
+const Filter = ({ newFilterName, handleFilterNameChange }) => {
+  return (
+    <div>
+      filter shown with<input value={newFilterName} onChange={handleFilterNameChange} />
+    </div>
+  )
+}
+
+const PersonForm = ({ addPerson, newName, newNumber, handleNameChange, handleNumberChange }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ persons, newFilterName }) => {
+  return persons.filter(x => x.name.includes(newFilterName.toUpperCase()) || x.name.includes(newFilterName.toLowerCase())).map((person) =>
+    <div key={person.name}>{person.name} {person.number}</div>
+  )
+
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilterName, setNewFilterName] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     console.log(persons)
     if (persons.every(currentValue => currentValue.name !== newName)) {
       setPersons(persons.concat({
-        name: newName
+        name: newName,
+        number: newNumber
       }))
       return
     }
@@ -24,22 +56,23 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterNameChange = (event) => {
+    setNewFilterName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map((person) =>
-        <div key={person.name}>{person.name}</div>
-      )
-      }
+      <Filter newFilterName={newFilterName} handleFilterNameChange={handleFilterNameChange} />
+      <h3>add a new</h3>
+      <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber}
+        handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
+      <h3>Numbers</h3>
+      <Persons persons={persons} newFilterName={newFilterName} />
     </div>
   )
 }
