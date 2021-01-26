7da50c65e45c7fe2bc6462a44f0eb23f337d1a8f
wusphinx <wu.sphinx@gmail.com>
2021-01-26 18:42:43 +0800

    done 2.13

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -10,9 +10,25 @@ const Filter = ({ newFilterName, handleFilterNameChange }) => {
 }
 
 const Country = ({ country }) => {
+  // TODO 
+  // useState也可以用于根组件App之外，之前还一直以为只能用于App组件之中
+  const [showAll, setShowAll] = useState(false)
+
   return (
     <div>
-      <h2>{country.name}</h2>
+      <h2>{country.name}<button onClick={() => {
+        if (showAll === false) { setShowAll(true) } else {
+          setShowAll(false)
+        }
+      }}>show</button></h2>
+      {showAll === true ? <Detail country={country} /> : <div></div>}
+    </div >
+  )
+}
+
+const Detail = ({ country }) => {
+  return (
+    <div>
       <div>capital {country.capital}</div>
       <div>population {country.population}</div>
       <h3>languages</h3>
@@ -22,7 +38,7 @@ const Country = ({ country }) => {
       <div>
         <img src={country.flag} alt="flag" width="90" height="120"></img>
       </div>
-    </div >
+    </div>
   )
 }
 
@@ -39,14 +55,8 @@ const Countries = ({ countries }) => {
     </div>
   }
 
-  if (countries.length === 1) {
-    return <div>
-      <Country country={countries[0]} />
-    </div>
-  }
-
   return countries.map((country) =>
-    <div key={country.name}>{country.name}</div>
+    <Country key={country.name} country={country} />
   )
 
 }
@@ -63,6 +73,10 @@ const App = () => {
         .get(`https://restcountries.eu/rest/v2/name/` + event.target.value)
         .then(response => {
           console.log('promise fulfilled')
+
+          response.data.forEach(element => {
+            element.isShow = false
+          });
           setNewCountries(response.data)
         })
     }
@@ -70,7 +84,6 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
       <Filter newFilterName={newFilterName} handleFilterNameChange={handleFilterNameChange} />
       <Countries countries={newCountries} />
     </div>
