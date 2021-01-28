c7bf7e16f754181550e9b9cd6550880eff7ccf0d
wusphinx <wu.sphinx@gmail.com>
2021-01-28 19:59:23 +0800

    done to 4.5

---
+++ part4/bloglist/tests/list_helper.test.js
@@ -0,0 +1,44 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+    const blogs = []
+
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(0)
+})
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+    })
+})
+
+describe('favorite likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('favoriteBlog', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        expect(result).toEqual(listWithOneBlog[0])
+    })
+})
---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,25 @@
+const blog = require("../models/blog")
+
+const dummy = (blogs) => {
+    return blogs.length
+}
+
+const totalLikes = blogs => blogs.reduce((acc, cur) => acc + cur.likes, 0)
+
+const favoriteBlog = blogs => {
+    const m = blogs[0]
+    for (let i = 1; i < blogs.length; i++) {
+        m = m.likes > blogs[i].likes ? m : blogs[i]
+    }
+
+    return m
+}
+
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+}
+
+
